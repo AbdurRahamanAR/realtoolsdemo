63976d639f16818342c8732294ca8ce5c9e18796
Abdur Rahaman <abdur@kimp.xyz>
2022-08-23 22:41:27 +0600

    Add theme

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,9 @@
 import React from 'react';
+import './styles/index.css'
 
 function App() {
   return (
-    <div>
+    <div className='text-dark font-bold bg-primary'>
       <h1>Hello</h1>
     </div>
   );
---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./src/**/*.{js,jsx,ts,tsx}'],
+  theme: {
+    colors: {
+      primary: 'var(--primary-color)',
+      secondary: 'var(--secondary-color)',
+      deep_orange: 'var(--deep-orange)',
+      light_orange: 'var(--light-orange)',
+      dark: '#000103',
+      white: '#ffffff',
+      sapphire: '#021850'
+    }
+  },
+  plugins: [],
+}
