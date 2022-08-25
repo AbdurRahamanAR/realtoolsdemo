c983870cfbdb6c1def2344503e28961e7efa277e
Abdur Rahaman <abdur@kimp.xyz>
2022-08-26 02:46:40 +0600

    Finish nav

--- src/components/TopBar.tsx
+++ src/components/TopBar.tsx
@@ -1,5 +1,6 @@
-import { Drawer } from "antd";
-import { Link } from "react-router-dom";
+import { Drawer, List } from "antd";
+import clsx from "clsx";
+import { NavLink } from "react-router-dom";
 import useToggle from "../hooks/useToggle";
 import Icon from "./Icon";
 import Logo from "./Logo";
@@ -31,12 +32,19 @@ export default function TopBar() {
         <ul className="hidden lg:flex gap-x-4 lg:gap-x-10 items-center">
           {navItems.map((navItem) => (
             <li key={navItem.label}>
-              <Link
-                className="text-white text-lg font-normal leading-5 hover:text-secondary hover:font-semibold hover:text-xl hover:leading-5"
+              <NavLink
+                className={({ isActive }) =>
+                  clsx([
+                    "leading-5 hover:text-secondary",
+                    isActive
+                      ? "text-secondary font-semibold text-xl"
+                      : "text-white text-lg font-normal",
+                  ])
+                }
                 to={navItem.url}
               >
                 {navItem.label}
-              </Link>
+              </NavLink>
             </li>
           ))}
           <li className="bg-secondary text-sapphire font-medium text-lg rounded-[10px] leading-5 py-2 px-[18px]">
@@ -45,7 +53,27 @@ export default function TopBar() {
         </ul>
 
         <Drawer onClose={() => toggle()} visible={showNavItems}>
-          <h1>Hello</h1>
+          <List
+            dataSource={navItems}
+            renderItem={(item) => (
+              <List.Item className="p-0">
+                <NavLink
+                  to={item.url}
+                  onClick={() => {
+                    toggle();
+                  }}
+                  className={({ isActive }) =>
+                    clsx([
+                      "py-4 pl-2 w-full h-full hover:bg-primary hover:text-white ",
+                      isActive ? "bg-primary text-white" : "bg-white text-dark",
+                    ])
+                  }
+                >
+                  {item.label}
+                </NavLink>
+              </List.Item>
+            )}
+          />
         </Drawer>
       </div>
     </div>
