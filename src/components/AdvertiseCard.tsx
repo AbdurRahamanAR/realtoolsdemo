58f4288c25fc961e075def21ceee49949c5b3224
Abdur Rahaman <abdur@kimp.xyz>
2022-08-27 19:20:59 +0600

    Create Advertise card

---
+++ src/components/AdvertiseCard.tsx
@@ -0,0 +1,30 @@
+import React from "react";
+import Icon from "./Icon";
+
+type AdvertiseCardProps = {
+  image: string;
+};
+
+const IconText = ({ text, icon }: { text: string; icon: string }) => {
+  return (
+    <span>
+      <Icon width={16} height={20.36} className="mr-2.5" icon={icon} />{" "}
+      <span>{text}</span>
+    </span>
+  );
+};
+
+export default function AdvertiseCard({ image }: AdvertiseCardProps) {
+  return (
+    <div className="rounded bg-alice_blue pt-[18.59px] pl-[22px] pr-[22.79px] pb-[24.71px]">
+      <img
+        className="w-full object-cover rounded max-h-[266.77px]"
+        src={image}
+        alt=""
+      />
+      <div>
+        <IconText icon="location" text="Jersy City, NJ" />
+      </div>
+    </div>
+  );
+}
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,26 +1,30 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./src/**/*.{js,jsx,ts,tsx}'],
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
+    borderRadius: {
+      DEFAULT: "10px",
+    },
     colors: {
-      primary: 'var(--primary-color)',
-      secondary: 'var(--secondary-color)',
-      deep_orange: 'var(--deep-orange)',
-      light_orange: 'var(--light-orange)',
-      dark: '#000103',
-      white: '#ffffff',
-      sapphire: '#021850'
+      primary: "var(--primary-color)",
+      secondary: "var(--secondary-color)",
+      deep_orange: "var(--deep-orange)",
+      light_orange: "var(--light-orange)",
+      dark: "#000103",
+      white: "#ffffff",
+      sapphire: "#021850",
+      alice_blue: "#F6F8FD",
     },
     fontFamily: {
-      'inter': 'Inter',
+      inter: "Inter",
     },
     screens: {
-      'sm': '640px',
-      'md': '768px',
-      'lg': '1024px',
-      'xl': '1245px',
-      '2xl': '1536px',
-    }
+      sm: "640px",
+      md: "768px",
+      lg: "1024px",
+      xl: "1245px",
+      "2xl": "1536px",
+    },
   },
   plugins: [],
-}
+};
