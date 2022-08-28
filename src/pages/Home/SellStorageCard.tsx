38efcb6c08e25d96b15ac1a15b500eb4fb1db14a
Abdur Rahaman <abdur@kimp.xyz>
2022-08-28 21:36:35 +0600

    Add sell section

--- src/pages/Home/SellStorageCard.tsx
+++ src/pages/Home/SellStorageCard.tsx
@@ -3,17 +3,34 @@ import Button from "../../components/Button";
 
 export default function SellStorageCard() {
   return (
-    <div className="bg-glitter">
-      <div>
-        <h3 className="text-heading font-bold text-sapphire mb-5">
-          Do you have a Self Storage for Sale?
-        </h3>
-        <p className="text-body text-dark">
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc tortor
-          sapien sagittis maecenas tincidunt. Quis pretium ornare id diam amet
-          proin scelerisque nunc.
-        </p>
-        <Button className="mt-10">Sell Your Self Storage</Button>
+    <div
+      className="
+        bg-glitter px-5 pt-10 pb-6
+        bg-no-repeat bg-left-bottom
+        bg-[url('../assets/sell_section_bg_shape.svg')]
+        rounded-br-[100px]
+        sm:pt-[100px] sm:px-[30px] sm:pb-[100px]
+        md:pt-[150px] md:pl-[50px] md:pb-[100px] 
+        md:rounded-br-[200px] bg-[position:left_bottom,_right_bottom]
+        md:bg-[length:auto,_400px]
+        md:bg-[url('../assets/sell_section_bg_shape.svg'),_url('../assets/man_with_container.png')]
+        lg:pt-[266px] lg:pl-[99px] lg:pb-[240px] 
+        lg:rounded-br-[300px]
+        lg:bg-[length:auto,_679px]
+      "
+    >
+      <div className="max-w-screen-xl mx-auto">
+        <div className="max-w-[638px]">
+          <h3 className="text-heading font-bold text-sapphire mb-5">
+            Do you have a Self Storage for Sale?
+          </h3>
+          <p className="text-body text-dark">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc tortor
+            sapien sagittis maecenas tincidunt. Quis pretium ornare id diam amet
+            proin scelerisque nunc.
+          </p>
+          <Button className="mt-10">Sell Your Self Storage</Button>
+        </div>
       </div>
     </div>
   );
